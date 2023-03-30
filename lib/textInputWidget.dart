da2075421170094a39e9d6a1505e2949915ce9da
Liza Kalbukova <lizakalbukova@gmail.com>
2023-03-30 09:31:27 +0300

    Форм коміт

--- lib/textInputWidget.dart
+++ lib/textInputWidget.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'transactionListW.dart';
-import 'mainColumnW.dart';
 
-class TextInputW extends StatelessWidget {
-  //String textInput = ''; // зміні які зберігають значення з рядку для ввода
-  //String amountInput = '';
-  final textController = TextEditingController();
-  final amountController = TextEditingController();
+class TextInputW extends StatefulWidget {
   final Function addNewTransaction;
   TextInputW(this.addNewTransaction);
+
+  @override
+  State<TextInputW> createState() => _TextInputWState();
+}
+
+class _TextInputWState extends State<TextInputW> {
+  //String textInput = ''; // зміні які зберігають значення з рядку для ввода
+  final textController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData() {
     // метод щоб наша кнопочка на клавіатурі підтвержувала і додавала транзакцію в лис .... ця функція створенна щоб код не повторювався
@@ -20,7 +25,10 @@ class TextInputW extends StatelessWidget {
       return;
     }
 
-    addNewTransaction(textSubmit, amountSubmit);
+    widget.addNewTransaction(textSubmit,
+        amountSubmit); // автоматично створено  widget. післа перетворення класу на StatefulWidget  для того щоб ми змогли передавати наші properties з приватного класа
+
+    Navigator.of(context).pop(); // Закриває нашу форму
   }
 
   @override
@@ -31,9 +39,7 @@ class TextInputW extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextField(
-              //onChanged:(value) { },
               onSubmitted: (_) => submitData(),
-
               decoration: InputDecoration(labelText: 'Name of transaction'),
               controller: textController,
             ),
