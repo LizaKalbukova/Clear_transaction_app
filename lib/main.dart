0e714cd5e3231182608e21ea421a1e4aa712e77a
Liza Kalbukova <lizakalbukova@gmail.com>
2023-03-30 09:21:56 +0300

    Додана форма

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'mainColumnW.dart';
+import 'textInputWidget.dart';
+import 'transactionListW.dart';
+import 'properties.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,6 +27,43 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  final List<MyTransactions> transctionW = [
+    MyTransactions(
+        amount: 32.99,
+        nameOfTransaction: "Banana",
+        id: "1m",
+        date: DateTime.now()),
+    MyTransactions(
+        amount: 12.99,
+        nameOfTransaction: "Cola",
+        id: "2m",
+        date: DateTime.now())
+  ];
+
+  void addNewTransaction(String txNameOfTx, double txAmount) {
+    final newTx = MyTransactions(
+      amount: txAmount,
+      nameOfTransaction: txNameOfTx,
+      id: DateTime.now().toString(),
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      transctionW.add(newTx);
+    });
+  }
+
+  void bottomFormInput() {
+    // метод для того щоб  вспливала форма з нашими імпутами
+    showModalBottomSheet(
+        context:
+            context, // те що ми передаємо в  showModalBottomSheet і будуємо форму
+        builder: (BuildContext bcontext) {
+          // bcontext це те куди передається context і ми повертаємо клас з нашими графічними віджетами
+          return TextInputW(addNewTransaction);
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
@@ -33,14 +71,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             Card(child: Text('Chart')),
-            MainColumnW(),
-            //   TransactionListW(),
+            //  MainColumnW(),
+            TransactionListW(transctionW)
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          bottomFormInput();
+        },
         child: const Icon(Icons.add),
       ),
     );
